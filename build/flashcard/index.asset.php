b465787fce7d9b59ff9aa08fa9650ac751a0ee06
Suraj Singh <suraj.sk243@gmail.com>
2025-02-02 19:11:06 +0530

    Background support for main root block

--- build/flashcard/index.asset.php
+++ build/flashcard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2679d9b38c998865d9e5');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2b6581cd4e38230cc80e');
