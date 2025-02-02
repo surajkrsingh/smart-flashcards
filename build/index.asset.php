99238b79a7290849d008058a110778fa2d39f30b
Suraj Singh <suraj.sk243@gmail.com>
2025-02-02 12:25:57 +0530

    Added styling properties

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '008b8573afb8b2e8617d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'd89686f992645cbb0873');
