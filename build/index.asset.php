4e602fe5d3c7bf6ada1005dad7309de36af80002
Suraj Singh <suraj.sk243@gmail.com>
2025-02-02 12:50:07 +0530

    Fix flipping issues on editor

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd5826acc84dc9f4ade32');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6eaca63c32ce86a4c8dc');
