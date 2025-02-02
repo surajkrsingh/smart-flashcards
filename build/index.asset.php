543b73e37118a36cf063ace865aa3ba2e5d3765b
Suraj Singh <suraj.sk243@gmail.com>
2025-02-02 12:20:14 +0530

    Fix flip text for flashcard

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c2051ffa260c34f1a078');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '008b8573afb8b2e8617d');
