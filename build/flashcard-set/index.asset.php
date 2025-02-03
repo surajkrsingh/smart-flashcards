d061d4eb4497af9f812f0303bcab9ce044ec172e
Suraj Singh <suraj.sk243@gmail.com>
2025-02-03 22:31:21 +0530

    Fix some issues on editor for flashcard-set

--- build/flashcard-set/index.asset.php
+++ build/flashcard-set/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '32d91aab3d3c20e578b9');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '325a1985b0e90b20da4d');
--- src/blocks/flashcard-set/edit.js
+++ src/blocks/flashcard-set/edit.js
@@ -1,8 +1,8 @@
 import { __ } from '@wordpress/i18n';
 import { useBlockProps, InnerBlocks } from '@wordpress/block-editor';
-import { Button, Tooltip } from '@wordpress/components';
+import { Button, ButtonGroup } from '@wordpress/components';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { trash } from '@wordpress/icons';
+import { trash, plus } from '@wordpress/icons';
 import { useEffect } from '@wordpress/element';
 import './editor.scss';
 
@@ -17,93 +17,85 @@ export default function Edit({ attributes, setAttributes, clientId }) {
         innerBlocks: select('core/block-editor').getBlocks(clientId)
     }), [clientId]);
 
-    const { removeBlock } = useDispatch('core/block-editor');
+    const { removeBlock, insertBlock } = useDispatch('core/block-editor');
     const totalSlides = innerBlocks?.length || 0;
 
-    // Hide all slides except current one
+    // Ensure currentSlide is valid
     useEffect(() => {
-        const slides = document.querySelectorAll(`#block-${clientId} .wp-block-smfcs-flashcard`);
-        slides.forEach((slide, index) => {
-            if (index === currentSlide) {
-                slide.style.display = 'block';
-            } else {
-                slide.style.display = 'none';
-            }
-        });
-    }, [currentSlide, totalSlides, clientId]);
+        if (currentSlide >= totalSlides) {
+            setAttributes({ currentSlide: Math.max(0, totalSlides - 1) });
+        }
+    }, [totalSlides]);
 
-    const handlePrev = () => {
-        setAttributes({ 
-            currentSlide: Math.max(0, currentSlide - 1)
-        });
+    const handleAddSlide = () => {
+        const block = wp.blocks.createBlock('smfcs/flashcard');
+        insertBlock(block, totalSlides, clientId);
+        setAttributes({ currentSlide: totalSlides });
     };
 
-    const handleNext = () => {
-        setAttributes({ 
-            currentSlide: Math.min(totalSlides - 1, currentSlide + 1)
-        });
-    };
-
-    const handleRemoveCard = () => {
+    const handleRemoveSlide = () => {
         if (totalSlides > 1) {
             const blockToRemove = innerBlocks[currentSlide];
             removeBlock(blockToRemove.clientId);
-            setAttributes({ 
+            setAttributes({
                 currentSlide: Math.max(0, currentSlide - 1)
             });
         }
     };
 
+    const selectSlide = (index) => {
+        setAttributes({ currentSlide: index });
+    };
+
     return (
-        <div {...blockProps} id={`block-${clientId}`}>
-            <div className="flashcard-set-wrapper">
+        <div {...blockProps}>
+            <div className="flashcard-set-editor">
+                {/* Slide Controls */}
                 <div className="flashcard-set-controls">
-                    <div className="flashcard-controls">
-                        <InnerBlocks.ButtonBlockAppender />
-                        {totalSlides > 1 && (
-                            <Tooltip text={__('Remove current flashcard', 'smart-flashcards')}>
+                    <div className="slide-selector">
+                        <div className="slide-selector-label">
+                            {__('Flashcards:', 'smart-flashcards')}
+                        </div>
+                        <ButtonGroup className="slide-buttons">
+                            {innerBlocks.map((_, index) => (
                                 <Button
-                                    variant="secondary"
-                                    icon={trash}
-                                    onClick={handleRemoveCard}
-                                    className="flashcard-remove-button"
-                                />
-                            </Tooltip>
+                                    key={index}
+                                    variant={currentSlide === index ? 'primary' : 'secondary'}
+                                    onClick={() => selectSlide(index)}
+                                    className="slide-button"
+                                >
+                                    {index + 1}
+                                </Button>
+                            ))}
+                            <Button
+                                icon={plus}
+                                variant="secondary"
+                                onClick={handleAddSlide}
+                                className="add-slide-button"
+                            />
+                        </ButtonGroup>
+                        {totalSlides > 1 && (
+                            <Button
+                                icon={trash}
+                                variant="secondary"
+                                onClick={handleRemoveSlide}
+                                className="remove-slide-button"
+                                isDestructive
+                            />
                         )}
-                    </div>
-                    <div className="flashcard-set-nav">
-                        <Button 
-                            variant="secondary"
-                            onClick={handlePrev}
-                            disabled={currentSlide === 0}
-                            icon="arrow-left-alt2"
-                            className="flashcard-nav-button prev"
-                        >
-                            {__('Previous', 'smart-flashcards')}
-                        </Button>
-                        <span className="flashcard-set-counter">
-                            {__('Card', 'smart-flashcards')} {currentSlide + 1} / {totalSlides}
-                        </span>
-                        <Button 
-                            variant="secondary"
-                            onClick={handleNext}
-                            disabled={currentSlide === totalSlides - 1}
-                            icon="arrow-right-alt2"
-                            iconPosition="right"
-                            className="flashcard-nav-button next"
-                        >
-                            {__('Next', 'smart-flashcards')}
-                        </Button>
                     </div>
                 </div>
 
-                <div className="flashcard-set-slides">
-                    <InnerBlocks
-                        allowedBlocks={ALLOWED_BLOCKS}
-                        template={TEMPLATE}
-                        orientation="horizontal"
-                        renderAppender={false}
-                    />
+                {/* Current Slide Editor */}
+                <div className="flashcard-editor">
+                    <div className="flashcard-slides">
+                        <InnerBlocks
+                            allowedBlocks={ALLOWED_BLOCKS}
+                            template={TEMPLATE}
+                            orientation="horizontal"
+                            renderAppender={false}
+                        />
+                    </div>
                 </div>
             </div>
         </div>
