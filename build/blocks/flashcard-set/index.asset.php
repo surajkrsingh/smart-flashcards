e90cb16fb77515f347c56e7c4d8be5716430160b
Suraj Singh <suraj.sk243@gmail.com>
2025-02-18 22:27:44 +0530

    Fix styles loading for flashcard

--- build/blocks/flashcard-set/index.asset.php
+++ build/blocks/flashcard-set/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'a07a3517d7cd203f83d7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4138cb935cbb1386f124');
--- build/blocks/flashcard/index.asset.php
+++ build/blocks/flashcard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0f7cb4b51a2e0f9eba22');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2ea0b95b6a12da8ffa35');
