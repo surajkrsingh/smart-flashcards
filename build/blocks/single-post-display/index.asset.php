a6c4cc0bf07f193a0b0ef5e41001d8617b2f4b28
Suraj Singh <suraj.sk243@gmail.com>
2025-03-01 20:07:25 +0530

    Fix some border styles

--- build/blocks/single-post-display/index.asset.php
+++ build/blocks/single-post-display/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => '48300f8abb2520e8f648');
+<?php return array('dependencies' => array('lodash', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives', 'wp-server-side-render'), 'version' => 'ad9ce9f29d55ff52d471');
