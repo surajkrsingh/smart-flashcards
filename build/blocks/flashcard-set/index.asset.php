e9e5b072b421e46d1f921a98927271fb0795c965
Suraj Singh <suraj.sk243@gmail.com>
2025-06-07 19:13:28 +0530

    Updated block icon

--- build/blocks/flashcard-set/index.asset.php
+++ build/blocks/flashcard-set/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0caf1c3928b97a6dadd7');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'db6e252a7e808b7b7abe');
--- build/blocks/flashcard/index.asset.php
+++ build/blocks/flashcard/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '99b8749b4b52d27d8265');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '83290ea5b8f5e0cb34eb');
